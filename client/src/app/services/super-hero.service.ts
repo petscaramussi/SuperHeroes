72527d24ee0bd7e4c73ed238b988b694e4e713be
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2023-07-31 16:57:56 -0300

    init

---
+++ client/src/app/services/super-hero.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { SuperHero } from '../models/super-hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SuperHeroService {
+
+  constructor() { }
+
+  public getSuperHeroes(): SuperHero[] {
+    let hero = new SuperHero();
+
+    hero.id = 1;
+    hero.name = "Spider Man";
+    hero.firstName = "Peter";
+    hero.lastName = "Paker";
+    hero.place = "New York City";
+
+    return [hero];
+  }
+}
